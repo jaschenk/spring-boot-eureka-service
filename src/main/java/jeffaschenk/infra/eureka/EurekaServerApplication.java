ba61b796a918be60e6f8464b7b097fcfaa89adc0
schenkje <Jeff.Schenk@modahealth.com>
2019-11-04 11:14:50 -0800

    Initial commit

---
+++ src/main/java/jeffaschenk/infra/eureka/EurekaServerApplication.java
@@ -0,0 +1,29 @@
+package jeffaschenk.infra.eureka;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableEurekaServer
+@EnableAdminServer
+public class EurekaServerApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
